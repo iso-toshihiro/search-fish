3c25fc18d7ee84a3f577aca5d088813eef9a2873
Hiro <hiro.bonvoyage@gmail.com>
2014-11-16 19:37:33 +0900

    generate controller diving_spots

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'diving_spots/index'
+
+  get 'diving_spots/show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
