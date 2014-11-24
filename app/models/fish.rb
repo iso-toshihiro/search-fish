13ebf7073961503e46ca99a183a5220612d99ff6
Hiro <hiro.bonvoyage@gmail.com>
2014-11-24 20:36:24 +0900

    make rerational table fish spots

--- app/models/fish.rb
+++ app/models/fish.rb
@@ -1,2 +1,4 @@
 class Fish < ActiveRecord::Base
+  has_many :fish_spots
+  has_many :spots, :through :fish_spots
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,4 @@
 class Spot < ActiveRecord::Base
+  has_many :fish_spots
+  has_many :fish, :through :fish_spot
 end
