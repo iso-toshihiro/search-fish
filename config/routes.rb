38a0da6eb3b104698a2d11da2897ac909c0412e1
Hiro <hiro.bonvoyage@gmail.com>
2014-11-29 20:21:34 +0900

    make route to diving_spot#show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'diving_spots/index'
 
-  get 'diving_spots/show'
+  get 'diving_spots/:id/fishes' => 'diving_spots#show', as: :fishes
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
