5f7604ad0c44d8a60d1e2a3c7823ded970d6eb62
Hiro <hiro.bonvoyage@gmail.com>
2015-01-19 23:51:02 +0900

    fix app/models/fish_spot.rb

--- app/models/fish_spot.rb
+++ app/models/fish_spot.rb
@@ -8,14 +8,14 @@ class FishSpot < ActiveRecord::Base
       fish_spot[:spot_id] = Spot.find_by_name(spot_name).id
       fish_spot[:fish_id] = Fish.find_by_name(fish_name).id
 
-      create(fish_spot) unless relation_exist?(fish_spot[:fish_id], fish_spot[:spot_id])
+      unless relation_exist?(fish_spot[:fish_id], fish_spot[:spot_id])
+        @fish_spot = create(fish_spot)
+        p "New relation fish_spot_id:#{@fish_spot.id} #{spot_name} - #{fish_name}"
+      end
     end
 
     def relation_exist?(fish_id, spot_id)
-      where(spot_id: spot_id).each do |fish_spot|
-        return true if fish_spot.fish_id == fish_id
-      end
-      false
+      where(spot_id: spot_id, fish_id: fish_id).present?
     end
   end
 end
