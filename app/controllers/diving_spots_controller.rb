04fd4f81042fe215251868ba7062e8edb65aef38
Hiro <hiro.bonvoyage@gmail.com>
2015-01-31 18:18:38 +0900

    add action select to diving_spots_controller

--- app/controllers/diving_spots_controller.rb
+++ app/controllers/diving_spots_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class DivingSpotsController < ApplicationController
   def index
     @spots = Spot.all
@@ -6,5 +7,25 @@ class DivingSpotsController < ApplicationController
   def show
     @spot = Spot.find(params[:id])
     @fish = @spot.fish
+    @groups = get_grops(@fish)
+  end
+
+  def selecte
+    fish = Spot.find(params[:id]).fish
+    all_ids = fish.map { |f| f.id }
+
+    display_ids = if params[:group] == 'すべて'
+                    all_ids
+                  else
+                    fish.where(group: params[:group]).map { |f| f.id }
+                  end
+    render json: { all_ids: all_ids, display_ids: display_ids }
+  end
+
+  private
+
+  def get_grops(fish)
+    grops = fish.map { |f| f.group }
+    grops.uniq
   end
 end
