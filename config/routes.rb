952707ea7d7536f92a4d6f7fec0289230c9e5fe9
Hiro <hiro.bonvoyage@gmail.com>
2015-02-11 18:00:30 +0900

    add route to diving_spots#coordinates

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get 'diving_spots/:id/fishes/group' => 'diving_spots#selecte'
 
   get 'diving_spots/search' => 'diving_spots#search'
+
+  get 'diving_spots/coordinates' => 'diving_spots#coordinates'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
